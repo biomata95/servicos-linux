b6eb7ff8e17391e4d10fad3a9f2bd2820b13a290
biomata95 <37086825+biomata95@users.noreply.github.com>
2019-03-10 18:25:18 -0300

    Update codigo-backup.sh

--- codigo-backup.sh
+++ codigo-backup.sh
@@ -1,3 +1,5 @@
+# A cada 1 minuto é gerado um backup
+
 function geraData(){
 	date=$(date +%F_%H-%M-%S)
 	echo "$date"
@@ -9,5 +11,5 @@ do
 	touch ../Backup/logs/$date.log
 	ls -R ../../Público/* >> ../Backup/logs/$date.log
 	mkdir ../Backup/$date && cp -r ../../Público/* ../Backup/$date
-	sleep 60
+	sleep 60 # pausa de 1 minuto (60 segundos)
 done
